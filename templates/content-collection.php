5dc8f69d16e73ad46575f82d11bfc010c4c661a9
Erik James Albaugh <erik@maintainweb.co>
2014-05-13 02:53:05 -0700

    Simple Transitions
    Transitions added to collections rollovers

--- templates/content-collection.php
+++ templates/content-collection.php
@@ -1,23 +1,26 @@
 <div class="row">
     <?php while (have_posts()) : the_post(); ?>
     <div class="col-sm-4">
-      <a href="<?php the_permalink(); ?>"><article <?php post_class(); ?>>
-        <header>
-          <h1 class="entry-title"><?php the_title(); ?></h1>
-        </header>
-        <div class="thumbnail-wrapper">
-        <?php 
-          if ( has_post_thumbnail() ) {
-            the_post_thumbnail('full', array('class' => 'img-responsive'));
-          } ?>
-        </div>
-        <div class="entry-content">
-          <?php // the_excerpt(); ?>
-        </div>
-        <footer>
+      <a href="<?php the_permalink(); ?>">
+        <article <?php post_class(); ?>>
+          <header>
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+          </header>
+          <div class="thumbnail-wrapper">
+          <?php 
+            if ( has_post_thumbnail() ) {
+              the_post_thumbnail('full', array('class' => 'img-responsive'));
+            } ?>
+          </div>
+          <div class="entry-content">
+            <?php // the_excerpt(); ?>
+          </div>
+          <footer>
 
-        </footer>
-      </article></a>
+          </footer>
+        </article>
+      </a>
     </div>
     <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
 </div>
